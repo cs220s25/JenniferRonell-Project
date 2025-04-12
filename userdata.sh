767f23f83ac0de38beb08ad6f9686593294a9364
Jennifer Alicea <148282022+JenniferAlicea@users.noreply.github.com>
2025-04-12 14:48:17 -0400

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-yum install git -y
+yum install -y maven-amazon-corretto21
 git clone https://github.com/cs220s25/JenniferRonell-Project /JenniferRonell-Project
 cd /JenniferRonell-Project
-python3 -m venv .venv
-.venv/bin/pip install -r requirements.txt 
-cp discord_bot.service /etc/systemd/system
-systemctl enable discord_bot.service 
+mvn clean package
+cp discord_bot.service /etc/systemd/system/
+systemctl enable discord_bot.service
 systemctl start discord_bot.service
