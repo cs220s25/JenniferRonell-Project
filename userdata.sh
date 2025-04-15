a59b766e30771db74d08a4f7e93f0def9059d857
Jennifer Alicea <aliceaj@moravian.edu>
2025-04-14 21:50:13 -0400

    Updated userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,7 +3,7 @@ yum install maven-amazon-corretto21 -y
 yum install git -y
 yum install redis6 -y
 
-git clone https://github.com/cs220s25/JenniferRonell-Project.git
+git clone https://github.com/cs220s25/JenniferRonell-Project /JenniferRonell-Project
 cd /JenniferRonell-Project
 mvn clean package
 cp discord_bot.service /etc/systemd/system/
