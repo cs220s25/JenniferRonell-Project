1a21051b79961a2a36f1d3916a3ee0a80ec437f5
Jennifer Alicea <148282022+JenniferAlicea@users.noreply.github.com>
2025-04-28 13:00:56 -0400

    Update redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -1,5 +1,5 @@
 cd /home/ec2-user/JenniferRonell-Project
-sudo systemctl stop discord_bot
+sudo systemctl stop discord_bot.service
 sudo git pull origin main
 mvn clean package
-sudo systemctl restart discord_bot
+sudo systemctl start discord_bot.service
