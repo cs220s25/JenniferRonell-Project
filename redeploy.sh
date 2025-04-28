94630bbd363319d40dda19c5ccd986fae8f2a736
Jennifer Alicea <148282022+JenniferAlicea@users.noreply.github.com>
2025-04-28 13:32:22 -0400

    Update redeploy.sh

--- redeploy.sh
+++ redeploy.sh
@@ -1,7 +1,6 @@
 cd /home/ec2-user/JenniferRonell-Project
 sudo systemctl stop discord_bot.service
-sudo pkill -f 'target/dbot-1.0-SNAPSHOT.jar' || true
 sudo rm -rf target/
 sudo git pull origin main
-mvn clean package
+sudo mvn clean package
 sudo systemctl start discord_bot.service
